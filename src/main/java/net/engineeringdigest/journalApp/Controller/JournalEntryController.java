ba8c5ee3cbf0cbf8a500fd746091a381910ab430
Anshuman Singh Sisodia <gagan.sisodiaknp786@gmail.com>
2026-01-16 00:11:30 +0530

    GET POST

--- src/main/java/net/engineeringdigest/journalApp/Controller/JournalEntryController.java
+++ src/main/java/net/engineeringdigest/journalApp/Controller/JournalEntryController.java
@@ -1,8 +1,27 @@
 package net.engineeringdigest.journalApp.Controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import net.engineeringdigest.journalApp.entity.JournalEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/journal")
 public class JournalEntryController {
 
+    private Map<Long, JournalEntry> journalentries = new HashMap<>();
+
+    @GetMapping
+    public List<JournalEntry> getAll(){  // localhost:8080/journal GET
+        return new ArrayList<>(journalentries.values());
+    }
+
+    @PostMapping
+    public boolean createEntry(@RequestBody JournalEntry myEntry){  // localhost:8080/journal POST
+        journalentries.put(myEntry.getId(),myEntry);
+        return true;
+    }
 }
---
+++ src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
@@ -0,0 +1,34 @@
+package net.engineeringdigest.journalApp.entity;
+
+public class JournalEntry {
+
+    private long id;
+
+    private String title;
+
+    private String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
