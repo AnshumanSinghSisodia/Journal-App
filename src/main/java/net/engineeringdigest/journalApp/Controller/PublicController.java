e42d8d59528599ab391acfef08c1c057d40def2c
Anshuman Singh Sisodia <gagan.sisodiaknp786@gmail.com>
2026-01-31 22:33:25 +0530

    spring security in user endpoints

---
+++ src/main/java/net/engineeringdigest/journalApp/Controller/PublicController.java
@@ -0,0 +1,25 @@
+package net.engineeringdigest.journalApp.Controller;
+
+import net.engineeringdigest.journalApp.Service.UserService;
+import net.engineeringdigest.journalApp.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/public")
+public class PublicController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/health-check")
+    public String Healthcheck(){
+        return "OK.......";
+    }
+
+
+    @PostMapping("/create-user")
+    public  void createUser(@RequestBody User user){
+        userService.saveEntry(user);
+    }
+}
