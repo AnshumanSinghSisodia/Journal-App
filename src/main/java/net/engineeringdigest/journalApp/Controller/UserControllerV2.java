0a4ae4efd78a0244cdf5d198f3486d55b0c3f0f2
Anshuman Singh Sisodia <gagan.sisodiaknp786@gmail.com>
2026-02-01 19:33:31 +0530

    role based authorization + properties/yml

---
+++ src/main/java/net/engineeringdigest/journalApp/Controller/UserControllerV2.java
@@ -0,0 +1,48 @@
+package net.engineeringdigest.journalApp.Controller;
+
+
+import net.engineeringdigest.journalApp.Service.UserService;
+import net.engineeringdigest.journalApp.entity.User;
+import net.engineeringdigest.journalApp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.Authenticator;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/user")
+public class UserControllerV2 {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PutMapping
+    public ResponseEntity<?> updateUser (@RequestBody User user){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userName = authentication.getName();
+        User userInDb=userService.findByUserName(userName);
+        userInDb.setUserName(user.getUserName());
+        userInDb.setPassword(user.getPassword());
+        userService.saveUser(userInDb);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/user")
+    public ResponseEntity<?> deleteUserById(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        userRepository.deleteByUserName(authentication.getName());
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+}
