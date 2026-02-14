3b79c11d9d6360ead6c8f75291231d66cc32bd11
Anshuman Singh Sisodia <gagan.sisodiaknp786@gmail.com>
2026-02-14 15:36:02 +0530

    @Value annotation

--- src/main/java/net/engineeringdigest/journalApp/Service/WeatherService.java
+++ src/main/java/net/engineeringdigest/journalApp/Service/WeatherService.java
@@ -1,7 +1,9 @@
 package net.engineeringdigest.journalApp.Service;
 
+import net.engineeringdigest.journalApp.Cache.AppCache;
 import net.engineeringdigest.journalApp.api.response.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -9,15 +11,20 @@ import org.springframework.web.client.RestTemplate;
 
 @Component
 public class WeatherService {
-    private static final String apiKey="2345923790b1b303fe365bdac979dc3d";
 
-    private static final String api="https://api.weatherstack.com/current?access_key=apiKey&query=CITY";
+    @Value("${weather.api.key}")
+    private String apiKey;
+
+   // private static final String api="https://api.weatherstack.com/current?access_key=apiKey&query=CITY";
 
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private AppCache appCache;
+
     public WeatherResponse getWeather(String city){
-        String finalApi = api.replace("CITY",city).replace("apiKey",apiKey);
+        String finalApi = appCache.APP_CACHE.get("weather_api").replace("<city>",city).replace("<apiKey>",apiKey);
         ResponseEntity<WeatherResponse> resp = restTemplate.exchange(finalApi, HttpMethod.GET,null, WeatherResponse.class);
         WeatherResponse body = resp.getBody();
         return body;
